a9e24e9d1bb65f62a09950d6fcdd3eeb7be75050
mgnolia <tnqls6560@gmail.com>
2020-12-18 16:50:30 +0900

    201218

---
+++ 201218_1/201218_1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _201218_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //실행:ctrl+F5
+            
+            //cw 입력 tap키 2번 = system.out.println
+            Console.WriteLine("hello 하고 한줄 줄바꿈");
+
+            //cw 입력 tap키 2번 후 Line 지우기 = system.out.print
+            Console.Write("hello 하고 줄 안바꿈");
+
+            //변수 : java와 동일 
+
+            int example = 100;
+            //단 Sring 변수는 String 이랑 string 둘 다 동일하게 쓸 수 있음
+            //c#에서는 보통 s를 소문자로 쓴다.
+            string ex1 = "hello";
+            String ex2 = "헬로";
+            string 이런것도됨 = "신기함";
+            Console.WriteLine(이런것도됨); // 근데 옛날 버전에선 안 돌아갈 수 있음.
+
+            //문자열과 숫자 혼합하기
+            string ex3 = example + "더하기" + example + "은" + (example + example) + "이다";
+            string ex4 = $"{example} 더하기 {example}은 {example + example}이다";
+            string ex5 = string.Format("{0} 더하기 {1}은 {2}이다", example, example, example+example);
+
+            Console.WriteLine(ex3);
+            Console.WriteLine(ex4);
+            Console.WriteLine(ex5);
+            
+            //조건문
+            //java와 동일
+            if (example > 50)
+            {
+                Console.WriteLine("example은 50이상);
+            }
+            if (example > 30)
+            {
+                Console.WriteLine("example은 30이상);
+
+            }
+            else
+            {
+                Console.WriteLine("example은 30미만");
+
+            }
+
+            if(example == 10)
+            {
+                Console.WriteLine("10);
+
+            }
+            else if(example == 20)
+            {
+                Console.WriteLine("20");
+
+                          }
+            if (example == 10)
+            {
+                Console.WriteLine("10");
+
+            }
+            else if (example == 20)
+            {
+                Console.WriteLine("20");
+            }
+            else
+            {
+                Console.WriteLine("10이랑 20은 아님");
+
+            }
+            switch (example)
+            {
+                case 10:
+                    Console.WriteLine("10");
+                    break;
+                case 20:
+                    Console.WriteLine("20");
+                    break;
+                default:
+                    Console.WriteLine("10이랑 20은 아니므니다");
+                    break;
+            }
+
+            //반복문도java와 동일
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("같은말 10번 반복 하기);
+            }
+            int temp = 10;
+
+            while (temp>0)
+            {
+                temp--;
+            }
+            do
+            {
+                temp++;
+            } while (temp < 10);
+
+            //값을 입력받은 경우
+            //java
+            /*
+                Scanner s = new Scanner(System.in);
+                String test = s.nextLine();
+                int test2 = s.nextIne;
+            */
+            //c#
+            // string test = Console.
+
+
+
+
+
+
+            
+
+
+            
+
+        }
+    }
+}
+
---
+++ 201218_1/201218_2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace _201218_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("문제풀이 시작");
+            Console.WriteLine("1번 문제");
+            int sum = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0)
+                    //sum = i;
+                    sum += i;
+            }
+            Console.WriteLine($"1부터 100까지의 수 중 3의 배수의 합은" + $"{sum}이다.");
+
+            Console.WriteLine("2번 문제");
+            int inputSecond = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{inputSecond / 60}분 {inputSecond % 60}초");
+
+            Console.WriteLine("3번문제");
+            Console.WriteLine("몇 층 까지 쌓을 것 인가");
+            int floor = int.Parse(Console.ReadLine());
+            for (int i = 0; i < floor; i++) // 전체 츶을 출력
+            {
+                for (int j = floor; j > i; j--) //각 층을 출력(공백)
+                {
+                    Console.Write(' ');
+                }
+                //별을 찍는 부분 //각 층을 출력(별)
+
+                for (int k = 0; k < (2 * i) + 1; k++)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("4번 문제");
+
+            for (int i = 2; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    Console.WriteLine($"{i}x{j}={i * j}");
+                }
+            }
+            Console.WriteLine("5번 문제");
+
+            int month = int.Parse(Console.ReadLine());
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("winter");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("spring");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("summer");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("fall");
+                    break;
+
+            }
+            Console.WriteLine("6번 문제");
+
+            int input1 = int.Parse(Console.ReadLine());
+            int input2 = int.Parse(Console.ReadLine());
+            if (input1 > 0 && input2 > 0)
+                Console.WriteLine("1");
+            else if (input1 > 0 && input2 > 0)
+                Console.WriteLine("2");
+            else if (input1 < 0 && input2 > 0)
+                Console.WriteLine("3");
+            else if (input1 < 0 && input2 < 0)
+                Console.WriteLine("4");
+            else
+                Console.WriteLine("0");
+
+
+        }
+
+        Console.WriteLine("7번 문제");
+
+        string start = "1";
+        for (int i = 0; i<length; i++)
+			{
+
+			}
+            //8 번 문제
+            Console.WriteLine("8번 문제);
+
+
+
+}
